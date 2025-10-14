a7a5ebf08a4de26d45818ef12f92776a2c34ea6a
Pratikshapandey16092003 <pratishapandey239@gmail.com>
2025-10-14 13:50:35 +0530

    commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import expennse from "../../Assets/Projects/expennse.png"
+import speakEasy from "../../Assets/Projects/SpeakEasy.png"
+import talentFlux from "../../Assets/Projects/TalentFlux.png"
+import astroai from "../../Assets/Projects/AstroAI.png"
 
 function Projects() {
   return (
     <Container fluid className="project-section">
-      <Particle />
+      {/* <Particle /> */}
       <Container>
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
@@ -20,18 +20,18 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={speakEasy}
               isBlog={false}
               title="Chat_App"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/Pratikshapandey1609/RealTime_ChatApp"
-              //demoLink=""
+            //demoLink=""
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={talentFlux}
               isBlog={false}
               title="Resume_Parsing"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
@@ -42,14 +42,26 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={expennse}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink=""              
+              title="Expense Tracker"
+              description="An interactive Expense Tracker Dashboard built with React.js and Tailwind CSS that visualizes income and expense insights using Recharts, featuring dynamic charts, smooth animations, and a responsive UI."
+              ghLink="https://github.com/Pratikshapandey1609/Expense-Tracker"
+              demoLink=""
             />
-          </Col>          
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={astroai}
+              isBlog={false}
+              title="Astro_AI"
+              description="An AI-powered Resume Analyzer built with the MERN stack that evaluates resumes, provides improvement suggestions, supports job-role-based analysis, and features a modern, animated, and multilingual UI with dark/light mode."
+              ghLink="https://github.com/Pratikshapandey1609/Astro_AI1"
+            //demoLink=""
+            />
+          </Col>
+
         </Row>
       </Container>
     </Container>
